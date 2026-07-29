3e1fb98b733cb20688d468dee261f4be3d19d107
j.j olveira 2009 <jorjinhocorrea@gmail.com>
2026-07-29 16:25:54 -0300

    fodase4

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -6,7 +6,7 @@ import { adminMiddleware } from "../middlewares/adminMiddleware";
 const router = Router()
 const controller = new UserController()
 
-router.get('/', authMiddleware, controller.findAllUser.bind(controller))
+router.get('/', controller.findAllUser.bind(controller))
 
 router.get('/:id', authMiddleware, adminMiddleware, controller.getUserById.bind(controller))
 router.get('/email/:email', authMiddleware, adminMiddleware, controller.getUserByEmail.bind(controller))
